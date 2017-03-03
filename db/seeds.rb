8ad7c54eff194661149ddd8cf9623cd4071306c9
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-03-03 15:05:55 -0500

    dummy data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.destroy_all
+Event.destroy_all
+Item.destroy_all
+
+10.times do
+    User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: Faker::Internet.password)
+end
+
+10.times do
+    Event.create(budget: Faker::Commerce.price, name: Faker::Name.title, user_id: Faker::Number.between(1, 10))
+end
+
+10.times do
+    Item.create(cost: Faker::Commerce.price, event_id: Faker::Number.between(1, 10), name: Faker::Pokemon.name )
+end
+
+# https://github.com/stympy/faker
